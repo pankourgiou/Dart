60cb61207cc31cd035981d8d3147eb6c79d810ba
pankourgiou <60981745+pankourgiou@users.noreply.github.com>
2021-09-04 21:36:52 +0300

    Create Socialdraama.py

---
+++ Socialdraama.py
@@ -0,0 +1,28 @@
+import time
+
+
+t1 = time.time()
+x = "Greeks are not bad people they are good people yet there is much negative energy and dark thoughts. We aren't worse than other cultures we just don't need all this self-criticism thing. It leads to 0 results. We need to get out all guns blazing, even if we are the cancel generation, we need to become extrovert"
+x1 = "democracy goes slow, there is a movement slow idea. Even in big enterprises movement isn't way too fast"
+x2 = "there is much silence these days like an omerta."
+x3 = "There are too many regulations on anything, that's not going to work"
+x4 = "We are the cancel generation, we want to drive without driving eat without eating the list is big...life this fashion is meaningless. IT's sad but true"
+x5 = "Cancel generation was here all the way back to the ancient times the idea is old,but we just quit easilly "
+x6 = "Movement on the roads today was normal"
+x7 = "There is much lack of respect in society. This doesn't help"
+x8 = "We need to keep pushing on action even if it's all about small profit."
+x9 = "I am ready for a bad case scenario..we are waiting for huge reaction from society."
+
+print(bool(x))
+print(bool(x1))
+print(bool(x2))
+print(bool(x3))
+print(bool(x4))
+print(bool(x5))
+print(bool(x6))
+print(bool(x7))
+print(bool(x8))
+print(bool(x9))
+t2 = time.time()
+t = t2 - t1
+print("Elapsed time is : ", t, " seconds")
