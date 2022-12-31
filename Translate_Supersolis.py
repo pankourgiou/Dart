917896dc1220ba40551aeef1da863cb3ebb3a948
pankourgiou <60981745+pankourgiou@users.noreply.github.com>
2022-12-31 03:15:41 +0200

    Update Translate_Supersolis.py

--- Translate_Supersolis.py
+++ Translate_Supersolis.py
@@ -1,4 +1,31 @@
 #Install translators module
+#the alphabet sounds from a-z are:
+#bli
+#ble
+#blo
+#bla
+#blu
+#bra
+#bre
+#bri
+#bro
+#bru
+#flu
+#fri
+#fro
+#gru
+#gri
+#gro
+#pli
+#ple
+#plo
+#pla
+#plu
+#pro
+#pru
+#sra
+#sre
+#sri
 import translators as ts
 
 text = 'cheese'
